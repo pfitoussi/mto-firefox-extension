1261ed7ca4aa2b4b64879fa51d1bb21cd5399583
pfitoussi <pierre@squadracer.com>
2023-01-17 16:09:20 +0100

    feat(comments): intercept request

--- src/background/background.js
+++ src/background/background.js
@@ -1,17 +1,60 @@
 import { filterInput, createUrl } from '../utils.js';
+const targets = [
+  'https://api-v2.soundcloud.com/*comments',
+  'https://api-v2.soundcloud.com/*comments?*',
+];
 
 browser.runtime.onInstalled.addListener(() => {
   browser.contextMenus.create({
     id: 'multiple-tab-opener',
-    type: 'radio',
+    type: 'normal',
     title: 'Open in multiple tab',
     contexts: ['selection'],
     checked: true,
   });
 });
+
 browser.runtime.onMessage.addListener((request) => {
   return openMultipleUrl(request);
 });
+
+browser.webRequest.onBeforeRequest.addListener(
+  ({ requestId }) => {
+    const filter = browser.webRequest.filterResponseData(requestId);
+    let bufferByRequest = {};
+    const decoder = new TextDecoder('utf-8');
+
+    filter.ondata = (event) => {
+      if (bufferByRequest.hasOwnProperty(requestId))
+        bufferByRequest[requestId].push(event.data);
+      else Object.assign(bufferByRequest, { [requestId]: [event.data] });
+      filter.write(event.data);
+    };
+
+    filter.onstop = (_) => {
+      const result = Object.values(bufferByRequest)
+        .map((value) => value)
+        .map((buffers) =>
+          buffers
+            .map((buffer) => decoder.decode(buffer, { stream: true }))
+            .join()
+        )
+        .map((payload) => JSON.parse(payload));
+
+      console.log(result);
+      filter.disconnect();
+    };
+
+    filter.onerror = (event) => {
+      console.log(`Error: ${filter.error}`);
+      console.log(event);
+    };
+
+    return {};
+  },
+  { urls: targets },
+  ['blocking']
+);
 
 browser.contextMenus.onClicked.addListener(
   async ({ menuItemId, selectionText }) => {
